6c6746086e4c3fc66831348a9014116d4931c794
lfernandez <alejandroferllub@gmail.com>
2023-08-13 21:12:51 -0400

    get current location

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Marker } from "react-native-maps";
 
 export default function App() {
+
+  const [location, setLocation] = React.useState();
+  const [adrees, setAdrees] = React.useState();
+
+  React.useEffect(() => {
+    const permission = async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+        return;
+      }
+
+      let currentLocation = await Location.getCurrentPositionAsync({});
+      setLocation(currentLocation);
+      console.log("current location ----------",currentLocation);
+    };
+    permission();
+  }, []);
+
+  const geocode = async () => {
+    const geolocaton = await Location.geocodeAsync(adrees);
+    console.log("geolocaton ----------",geolocaton);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput placeholder="Search location" value={adrees} onChangeText={setAdrees}/>
+      <Button title="search adrees" onPress={geocode} />
+      <View style={styles.mapContainer}>
+        <MapView 
+          style={styles.map}
+          initialRegion={{
+            latitude: location?.coords?.latitude,
+            longitude: location?.coords?.longitude,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01,
+          }}>
+            {/* <Marker coordinate={{
+              latitude: location?.coords?.latitude,
+              longitude: location?.coords?.longitude,
+              latitudeDelta: 0.01,
+              longitudeDelta: 0.01,
+            }} /> */}
+        </MapView>
+      </View>
     </View>
   );
 }
@@ -13,8 +57,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:'center',
+  },
+  mapContainer: {
+    width: '100%',
+    height: '40%',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+  navBar: {
+    backgroundColor: 'red',
   },
 });
